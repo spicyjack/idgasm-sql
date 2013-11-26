6c76746705e48f77121c61fbae4781a72b4702fd
Brian Manning <brian@xaoc.org>
2013-11-25 16:44:27 -0800

    JSONParser: close #31; JSON object parses and returns File objects

--- App-idgasmTools/lib/App/idgasmTools/JSONParser.pm
+++ App-idgasmTools/lib/App/idgasmTools/JSONParser.pm
@@ -14,23 +14,14 @@ use JSON;
 use Log::Log4perl qw(get_logger :no_extra_logdie_message);
 use Moo;
 
+use Data::Dumper;
+$Data::Dumper::Indent = 1;
+$Data::Dumper::Sortkeys = 1;
+$Data::Dumper::Terse = 1;
+
 # local modules
 use App::idgasmTools::Error;
-
-=head2 Methods
-
-=over
-
-=item parsing_module
-
-The Perl module that was used to parse the XML data.
-
-=cut
-
-has q(parsing_module) => (
-    is      => q(ro),
-    default => q(JSON),
-);
+use App::idgasmTools::File;
 
 =head2 Methods
 
@@ -38,11 +29,10 @@ has q(parsing_module) => (
 
 =item parse(data => $response->content)
 
-Parses the content inside of the HTTP response message sent from the server in
-response to an C<idGames Archive API> request.  Returns a reference to a
-complex data structure that represents the parsed data if parsing was
-successful, or a L<App::idgasmTools::Error> object if parsing was not
-successful.
+Parses the JSON content inside of the HTTP response message sent from the
+server in response to an C<idGames Archive API> request.  Returns an
+L<App::idgasmTools::File> object if parsing was successful, or a
+L<App::idgasmTools::Error> object if parsing was not successful.
 
 =cut
 
@@ -58,14 +48,46 @@ sub parse {
     my $json = JSON::XS->new->utf8->pretty->allow_unknown;
     # wrap decode() in an eval to handle parsing errors
     my $parsed_data = eval{$json->decode($data);};
-    $log->debug(q(Dumping ) . $self->parse_module . qq( output:\n)
-        . Dumper $parsed_data);
     if ( $@ ) {
-        my $error = App::idgasmTools::Error->new(error_msg => $@);
+        my $error = App::idgasmTools::Error->new(
+            error_msg => qq(Error parsing JSON content; $@),
+        );
         return $error;
     } else {
-        return $parsed_data;
+        #$log->debug(q(Dumping JSON parser output:));
+        #$log->debug(Dumper $parsed_data);
+        if ( exists $parsed_data->{content} ) {
+            my $content = $parsed_data->{content};
+            #$log->warn(qq(Dumping content:\n) . Dumper($content));
+            my $file = App::idgasmTools::File->new();
+            # go through all of the attributes in the content object, copy
+            # them to the same attributes in this File object
+            my @attribs = @{$file->file_attributes};
+            $log->debug(q(Populating File attributes...));
+            foreach my $key ( @attribs ) {
+                if ( defined $content->{$key} ) {
+                    $file->{$key} = $content->{$key};
+                } else {
+                    $file->{$key} = q();
+                }
+                next if ( $key eq q(textfile) );
+                $log->debug(qq(  $key: >) . $file->$key . q(<));
+            }
+            return $file
+        } elsif ( exists $parsed_data->{error} ) {
+            my $error = App::idgasmTools::Error->new(
+                error_msg => q(Received 'error' response to API query),
+                content_block => $parsed_data->{error},
+            );
+            return $error;
+        } else {
+            my $error = App::idgasmTools::Error->new();
+            $error->error_msg(q(Received undefined response to API query));
+            return $error;
+        }
     }
+    # we shouldn't get this far
+    $log->logdie(q(JSONParser reached end of parse block without branching));
 }
 
 1;

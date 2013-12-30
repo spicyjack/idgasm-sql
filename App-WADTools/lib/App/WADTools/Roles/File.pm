24b0c9fba6c999575f97f7700088dfe7443719e8
Brian Manning <brian@xaoc.org>
2013-12-30 00:38:23 -0800

    Roles/File: issue #108; begin implementing file, updated POD

--- App-WADTools/lib/App/WADTools/Roles/File.pm
+++ App-WADTools/lib/App/WADTools/Roles/File.pm
@@ -1,44 +1,68 @@
-##########################
-# App::WADTools::ZipTool #
-##########################
-package App::WADTools::ZipTool;
+##############################
+# App::WADTools::Roles::File #
+##############################
+package App::WADTools::Roles::File;
 
 =head1 NAME
 
-App::WADTools::ZipFile
+App::WADTools::Roles::File
 
 =head1 SYNOPSIS
 
-This object keeps track of a file compressed in C<.zip> format.  This object
-will read the contents of the zip file, as well as obtain checksums of the
-zipfile.
+Common Methods dealing with file handling.
 
 =head1 DESCRIPTION
 
-Provides a listing of the zipfile's contents, and checksums of the zipfile.
+This role contains methods common to both L<App::WADTools::ZipFile> and
+L<App::WADTools::WADFile>, including getting file attributes from the
+filesystem (file size, creation date, etc) and checksumming files.
 
 =cut
-use Archive::Zip qw(:ERROR_CODES);
+
+### System modules
+# 'Moo::Role' calls 'strictures', which is 'strict' + 'warnings'
+use Moo::Role;
 use Digest::MD5;
 use Digest::SHA;
 use Log::Log4perl;
-# 'Moo' calls 'strictures', which is 'strict' + 'warnings'
-use Moo;
 
 =head2 Attributes
 
 =over
 
-=item zipfile
+=item file
 
-The zipfile to work with.
-
-=back
+The file that this role will work with.
 
 =cut
 
-has q(zipfile) => (
+has q(file) => (
     is => q(rw),
+    # TODO check for a valid file here
+    #isa
+);
+
+=item md5_checksum
+
+The file's MD5 checksum
+
+=cut
+
+has q(md5_checksum) => (
+    is => q(rw),
+    default => sub{ q() },
+    #isa
+);
+
+=item sha_checksum
+
+The file's SHA1 checksum.
+
+=cut
+
+has q(sha_checksum) => (
+    is => q(rw),
+    default => sub{ q() },
     #isa
 );
 
@@ -46,95 +70,57 @@ has q(zipfile) => (
 
 =over
 
-=item new(zipfile => $zipfile) (aka BUILD)
+=item gen_md5_checksum
 
-Creates a L<App::WADTools::ZipFile> object, and populates the zipfile's object
-attributes, including MD5/SHA checksums.
+Generates the MD5 checksum of the file stored in the C<file> attribute, stores
+the checksum in the C<md5_checksum> attribute, and also returns it to the
+caller.
 
 =cut
 
-sub BUILD {
-    my $class = shift;
-    my %args = @_;
-    my $self = bless ({%args}, $class);
+sub gen_md5_checksum {
+    my $self = shift;
     my $log = Log::Log4perl->get_logger(""); # "" = root logger
-
-    my $zip = Archive::Zip->new();
-    my $zipfile = $self->zipfile;
-    $log->debug(qq(Reading zipfile: $zipfile));
-    $log->logdie(qq(Can't read zipfile $zipfile))
-        unless ( $zip->read($zipfile) == AZ_OK );
-    $self->{_zip} = $zip;
-    $log->debug("Calling zip->members");
-    my @member_objs = $zip->members();
-    my @members;
-    foreach my $member ( @member_objs ) {
-        push(@members, $member->fileName);
-    }
-    $self->{_members} = \@members;
-    return $self;
 }
 
-=item get_zip_members( )
+=item gen_sha_checksum
 
-Returns all of the files contained inside of the zipfile.
-
-=cut
-
-sub get_zip_members {
-    my $self = shift;
-    my $zip = $self->{_zip};
-    my $log = Log::Log4perl->get_logger(""); # "" = root logger
-
-    return @{$self->{_members}};
-}
-
-=item extract_files(files => \@files)
-
-Extracts all of the files listed in the array C<@files> from the zipfile and
-returns a scalar containing the path to the temporary directory that the files
-were extracted into.
+Generates the SHA checksum of the file stored in the C<file> attribute, stores
+the checksum in the C<sha_checksum> attribute, and also returns it to the
+caller.
 
 =cut
 
-sub extract_files {
+sub gen_sha_checksum {
     my $self = shift;
-    my %args = @_;
     my $log = Log::Log4perl->get_logger(""); # "" = root logger
-
-    my $zip = $self->{_zip};
-    my $tempdir = $args{tempdir};
-
-    my %file_temp_opts;
-    if ( defined $tempdir ) {
-        %file_temp_opts = (
-            DIR         => $tempdir,
-            TEMPLATE    => qq(wadindex.XXXXXXXX),
-        );
-    } else {
-        %file_temp_opts = (
-            TMPDIR      => 1,
-            TEMPLATE    => qq(wadindex.XXXXXXXX),
-        );
-    }
-    my $dh = File::Temp->newdir(%file_temp_opts);
-
-    $log->debug(qq(Created temp dir ) . $dh->dirname);
-    foreach my $file ( @{$args{files}} ) {
-        $log->debug(qq(- extracting: $file));
-        my $temp_file = $dh->dirname . q(/) . $file;
-        my $unzip_status = eval{$zip->extractMemberWithoutPaths(
-            $file, $temp_file);};
-        if ( $unzip_status != AZ_OK ) {
-            $log->error(qq(Could not unzip $file));
-            return undef;
-        }
-        $log->debug(q(- done extracting: ) . $file);
-    }
-    return $dh;
 }
 
 =back
+
+=head1 AUTHOR
+
+Brian Manning, C<< <brian at xaoc dot org> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to the GitHub issue tracker for
+this project:
+
+C<< <https://github.com/spicyjack/wadtools/issues> >>.
+
+=head1 SUPPORT
+
+You can find documentation for this script with the perldoc command.
+
+    perldoc App::WADTools::Roles::File
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright (c) 2013 Brian Manning, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
 
 =cut
 

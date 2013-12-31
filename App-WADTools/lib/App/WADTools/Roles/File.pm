b5e161b3935ee2e782100ca33788eb75316ba560
Brian Manning <brian@xaoc.org>
2013-12-31 14:43:13 -0800

    Roles/File: issue #108; implement checksumming, filehandles
    - Added method `generate_filehandle()` to create an `IO::File` object
      and store it in the `filehandle` attribute
    - Added method `pad_base64_digest` to pad out a digest value to an
      even 4 byte length
    - Added implementations of MD5 and SHA1 checksums

--- App-WADTools/lib/App/WADTools/Roles/File.pm
+++ App-WADTools/lib/App/WADTools/Roles/File.pm
@@ -30,16 +30,30 @@ use Log::Log4perl;
 
 =over
 
-=item file
+=item filename
 
-The file that this role will work with.
+The name of the file as it exists on the filesystem.
 
 =cut
 
-has q(file) => (
+has q(filename) => (
     is => q(rw),
     # TODO check for a valid file here
     #isa
+);
+
+=item filehandle
+
+A filehandle to the file, created as an L<IO::File> object (which inherts from
+L<IO::Handle>).
+
+=cut
+
+has q(filehandle) => (
+    is   => q(rw),
+    isa  => sub {
+                die qq('filehandle' is not an 'IO::File', but a ) . ref($_[0])
+                    unless ref($_[0]) eq q(IO::File) },
 );
 
 =item md5_checksum
@@ -66,9 +80,29 @@ has q(sha_checksum) => (
     #isa
 );
 
+=back
+
 =head2 Methods
 
 =over
+
+=item generate_filehandle
+
+Creates a read-only L<IO::File> object using the C<filename> attribute, and
+stores it in the C<filehandle> attribute for other objects to use.  Also sets
+C<binmode> on the filehandle.
+
+=cut
+
+sub generate_filehandle {
+    my $self = shift;
+    my $log = Log::Log4perl->get_logger(""); # "" = root logger
+
+    #$log->debug(q(filename: ) . $self->filename);
+    $self->filehandle(IO::File->new($self->filename, q(r)));
+    #$log->debug(q(filehandle isa: ) . ref($self->filehandle));
+    $self->filehandle->binmode;
+}
 
 =item gen_md5_checksum
 
@@ -81,6 +115,14 @@ caller.
 sub gen_md5_checksum {
     my $self = shift;
     my $log = Log::Log4perl->get_logger(""); # "" = root logger
+
+    my $md5 = Digest::MD5->new();
+    $md5->addfile($self->filehandle);
+    my $digest = $md5->b64digest;
+    # pad the digest string as needed...
+    $digest = $self->pad_base64_digest($digest);
+    $self->md5_checksum($digest);
+    return $digest;
 }
 
 =item gen_sha_checksum
@@ -94,6 +136,36 @@ caller.
 sub gen_sha_checksum {
     my $self = shift;
     my $log = Log::Log4perl->get_logger(""); # "" = root logger
+
+    my $sha = Digest::SHA->new(q(sha1));
+    $sha->addfile($self->filehandle);
+    my $digest = $sha->b64digest;
+    # pad the digest string as needed...
+    $digest = $self->pad_base64_digest($digest);
+    $self->sha_checksum($digest);
+    return $digest;
+}
+
+=item pad_base64_digest($base64_string)
+
+Pads the output of an MD5 or SHA digest that was output as a Base64 string, so
+that other implementations of Base64 will parse this Base64 string correctly.
+Implementation taken from comments/code in the C<Digest::SHA> module.  Accepts
+a scalar containing the Base64 string to be padded, returns the string padded
+with the equals sign C<=> as needed to make the length of the file divisible
+by 4.
+
+=cut
+
+sub pad_base64_digest {
+    my $self = shift;
+    my $base64 = shift;
+    my $log = Log::Log4perl->get_logger(""); # "" = root logger
+
+    while (length($base64) % 4) {
+        $base64 .= '=';
+    }
+    return $base64;
 }
 
 =back

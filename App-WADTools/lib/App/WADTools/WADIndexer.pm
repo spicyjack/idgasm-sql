e1b82e93854b13ea9bca9f0d84dbb1e1f61467c6
Brian Manning <brian@xaoc.org>
2014-01-12 22:34:04 -0800

    WADIndexer: added new method index_wad()
    - index_wad() indexes one WAD file
    - Renamed existing method index() to index_wad_list(), which
      enumerates over a list of WAD files, and calls index_wad() on each
      file

--- App-WADTools/lib/App/WADTools/WADIndexer.pm
+++ App-WADTools/lib/App/WADTools/WADIndexer.pm
@@ -10,7 +10,10 @@ App::WADTools::WADIndexer
 =head1 SYNOPSIS
 
  my $indexer = App::WADTools::WADIndexer->new();
- my $wadfile = $indexer->index(wadfile => q(/path/to/file.wad);
+ my $wadfile = $indexer->index_wad(
+    path     => q(/path/to/wadfiles),
+    filename => q(file.wad),
+ );
  print q(Levels in this WAD file: ) . join($wadfile->levels) . qq(\n);
 
 =head1 DESCRIPTION
@@ -24,7 +27,7 @@ data as an C<App::WADTools::WADFile> object to the caller.
 # system modules
 # 'Moo' calls 'strictures', which is 'strict' + 'warnings'
 use Data::Hexdumper;
-use Fcntl qw(:seek);
+use Fcntl qw(:seek); # for random byte access to files
 use Log::Log4perl;
 use Moo;
 
@@ -33,50 +36,40 @@ use constant {
     WAD_HEADER_SIZE          => 12,
 };
 
+my $level_lump_regex = qr/MAP[0-4][0-9]|E[1-4]M[1-9]/;
+
 =head2 Attributes
+
+No object attributes (currently).
 
 =over
 
-=item filename
+=item foo
 
-A filename to the C<SQLite> database file.  If the file does not exist, a new
-file will be created.
+A bogus object attribute.
 
 =back
 
 =cut
 
-has idx_db => (
-    is      => q(rw),
-    default => sub{ undef; },
+#has foo => (
+#    is      => q(rw),
+#    default => sub{ undef; },
     # returns true if the argument is a DBI object
     #isa => sub { (ref($_[0]) eq q(DBI)) ? 1 : 0 },
-);
+#);
 
 =head2 Methods
 
 =over
 
-=item new(idx_db => $dbh) (aka BUILD)
+=item new() (aka BUILD)
 
 Creates a new WADIndexer object, returns it to the caller.
 
-Optional arguments:
+=item index_wad_list(unzip_dir => $dir, $files => \@files)
 
-=over
-
-=item idx_db
-
-The L<DBI> database handle to an existing C<index> database that has already
-had the C<connect()> method called on it.  The database is usually created
-with the C<db_tool> script and the C<wad_index.ini> schema file.
-
-=back
-
-=item index(unzip_dir => $dir, $files => \@files)
-
-Indexes the contents of a WAD file, and displays the information on the
-screen.
+Indexes a list of WAD files, and displays the information on the screen.
 
 Required arguments:
 
@@ -89,13 +82,13 @@ A path to the directory where the C<.zip> file was unzipped
 =item files
 
 A reference to an array of filenames that the method should index and
-optionally, add to the database.
+return to the caller.
 
 =back
 
 =cut
 
-sub index {
+sub index_wad_list {
     my $self = shift;
     my %args = @_;
     my $log = Log::Log4perl->get_logger(""); # "" = root logger
@@ -105,68 +98,127 @@ sub index {
     $log->logdie(q(Missing 'files' argument))
         unless ( defined $args{files} );
 
+    my @wadlist;
     FILE: foreach my $filename ( @{$args{files}} ) {
         # skip dotfiles
         if ( $filename =~ m!/\.\w+! ) {
             $log->debug(qq(Skipping dotfile '$filename'));
             next FILE;
         }
-        my $wadfile = $args{unzip_dir} . q(/) . $filename;
-        $log->info(qq(Reading WAD info from '$filename'));
-        open(my $WAD, qq(<$wadfile))
-            or $log->logdie(qq(Failed to open WAD file '$wadfile': $!));
-        my $header;
-        # read the header from the WAD file
-        my $bytes_read = read( $WAD,$header, WAD_HEADER_SIZE );
-        $log->logdie(qq(Failed to read header: $!))
-            unless (defined $bytes_read);
-        if ( $bytes_read != WAD_HEADER_SIZE ) {
-            $log->error(qq(Only read $bytes_read bytes from header));
-            $log->logdie(q(Header size is ) . WAD_HEADER_SIZE . q( bytes));
-        }
-        my ($wad_sig,$num_lumps,$dir_offset) = unpack("a4VV",$header);
-        $log->info(qq(WAD signature: $wad_sig));
-        $log->info(sprintf(q(Number of lumps in the WAD:  %u lumps),
-            $num_lumps));
-        $log->debug(sprintf(q(WAD directory start offset: +%u bytes),
-            $dir_offset));
-        for (my $i = 0; $i <= ($num_lumps - 1); $i++) {
-            my $lump_entry;
-            # reset bytes read
-            $bytes_read = undef;
-            # read this lump entry
-            $log->debug(q(Reading directory entry at offset: )
-                . ($dir_offset + ( $i * WAD_DIRECTORY_ENTRY_SIZE )));
-            die(qq(Can't seek WAD directory entry: $!))
-                unless (seek($WAD,
-                    ($dir_offset
-                    + ( $i * WAD_DIRECTORY_ENTRY_SIZE )),
-                    SEEK_SET));
-            $bytes_read = read($WAD, $lump_entry, WAD_DIRECTORY_ENTRY_SIZE);
-            die "Failed to read WAD directory entry: $!"
-                unless ( defined $bytes_read );
-            die qq(Only read $bytes_read out of ) . WAD_DIRECTORY_ENTRY_SIZE
-                . q( bytes in header)
-                unless ( $bytes_read == WAD_DIRECTORY_ENTRY_SIZE );
-            my ($hex_chars, $data) = split(/::/,
-                hexdump(
-                    data => $lump_entry,
-                    output_format => q(%16C::%d),
-            ));
-            $log->debug(qq(-> lump raw data: $data));
-            # nice header for displaying lump directory entry info
-            $log->debug(
-                qq(->|lump start | lump size | lump name             |));
-            $log->debug(qq(-> $hex_chars));
-
-            my ($lump_start, $lump_size, $lump_name) = unpack(q(VVa8),
-                $lump_entry );
-            $lump_name =~ s/\0+//g;
-            $log->info(sprintf(q(lump %-4u name: %-8s size: %-8u start: %-8x),
-                $i + 1, $lump_name, $lump_size, $lump_start));
-        }
-        close($WAD);
+        my $wadpath = $args{unzip_dir} . q(/) . $filename;
+        my $wadfile_obj = $self->index_wad(wadpath => $wadpath);
+        push(@wadlist, $wadfile_obj);
     }
+    return @wadlist;
+}
+
+=item index_wad(wadpath => q(/path/to/file.wad))
+
+Indexes the contents of a WAD file, and displays the information on the
+screen.  Returns an L<App::WADTools::WADFile> object if the WAD file argument
+could be read and parsed, returns C<undef> otherwise.
+
+Required arguments:
+
+=over
+
+=item wadpath
+
+The path to a WAD file.
+
+=back
+
+=cut
+
+sub index_wad {
+    my $self = shift;
+    my %args = @_;
+    my $log = Log::Log4perl->get_logger(""); # "" = root logger
+
+    $log->logdie(q(Missing 'path' argument))
+        unless ( defined $args{path} );
+    $log->logdie(q(Missing 'wad_filename' argument))
+        unless ( defined $args{wad_filename} );
+
+    # create local variables from method arguments
+    my $filename = $args{wad_filename};
+    my $path = $args{path};
+    my $wad_path = $path . q(/) . $filename;
+
+    # skip dotfiles
+    if ( $filename =~ m!/\.\w+! ) {
+        $log->debug(qq(Skipping dotfile '$filename'));
+        return undef;
+    }
+
+    $log->info(qq(Reading WAD info from '$filename'));
+    open(my $WAD, qq(<$wad_path))
+        or $log->logdie(qq(Failed to open WAD file '$wad_path': $!));
+    my $header;
+    # read the header from the WAD file
+    my $bytes_read = read( $WAD,$header, WAD_HEADER_SIZE );
+    $log->logdie(qq(Failed to read header: $!))
+        unless (defined $bytes_read);
+    if ( $bytes_read != WAD_HEADER_SIZE ) {
+        $log->error(qq(Only read $bytes_read bytes from header));
+        $log->logdie(q(Header size is ) . WAD_HEADER_SIZE . q( bytes));
+    }
+    my ($wad_sig,$num_lumps,$dir_offset) = unpack("a4VV",$header);
+    $log->info(qq(WAD signature: $wad_sig));
+
+    # create the WADFile object here
+    my $wadfile = App::WADTools::WADFile->new();
+    $wadfile->wad_signature($wad_sig);
+    $wadfile->num_of_lumps($num_lumps);
+
+    $log->info(sprintf(q(Number of lumps in the WAD:  %u lumps),
+        $num_lumps));
+    $log->debug(sprintf(q(WAD directory start offset: +%u bytes),
+        $dir_offset));
+    for (my $i = 0; $i <= ($num_lumps - 1); $i++) {
+        my $lump_entry;
+        # reset bytes read
+        $bytes_read = undef;
+        # read this lump entry
+        $log->debug(q(Reading directory entry at offset: )
+            . ($dir_offset + ( $i * WAD_DIRECTORY_ENTRY_SIZE )));
+        $log->logdie(qq(Can't seek WAD directory entry: $!))
+            unless (seek($WAD,
+                ($dir_offset
+                + ( $i * WAD_DIRECTORY_ENTRY_SIZE )),
+                SEEK_SET));
+        $bytes_read = read($WAD, $lump_entry, WAD_DIRECTORY_ENTRY_SIZE);
+        $log->logdie "Failed to read WAD directory entry: $!"
+            unless ( defined $bytes_read );
+        $log->logdie qq(Only read $bytes_read out of )
+            . WAD_DIRECTORY_ENTRY_SIZE
+            . q( bytes in header)
+            unless ( $bytes_read == WAD_DIRECTORY_ENTRY_SIZE );
+        # use split to parse the output of a hexdump with a special format
+        my ($hex_chars, $data) = split(/::/,
+            hexdump(
+                data => $lump_entry,
+                output_format => q(%16C::%d))
+        );
+        $log->debug(qq(-> lump raw data: $data));
+        # nice header for displaying lump directory entry info
+        $log->debug(
+            qq(->|lump start | lump size | lump name             |));
+        $log->debug(qq(-> $hex_chars));
+
+        # get the start, size and name of the lump via unpack()
+        my ($lump_start, $lump_size, $lump_name) = unpack(q(VVa8),
+            $lump_entry );
+        # strip trailing 'NUL' characters
+        $lump_name =~ s/\0+//g;
+        # FIXME
+        # - detect lump name here using the regex
+        # - add level lumps to WADFile
+        $log->info(sprintf(q(lump %-4u name: %-8s size: %-8u start: %-8x),
+            $i + 1, $lump_name, $lump_size, $lump_start));
+    }
+    close($WAD);
+
 }
 
 =back

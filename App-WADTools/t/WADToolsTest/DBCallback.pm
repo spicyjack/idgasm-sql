5d4fe05b8aff17dc52bab34c219cd44b05f3c346
Brian Manning <brian@xaoc.org>
2014-02-22 11:34:21 -0800

    project: issue #155; added test DB Callback object

---
+++ App-WADTools/t/WADToolsTest/DBCallback.pm
@@ -0,0 +1,32 @@
+#!perl -T
+
+package WADToolsTest::DBCallback;
+# Copyright (c) 2014 Brian Manning <brian at xaoc dot org>
+# For support with this file, please file an issue on the GitHub issue tracker
+# for this project: https://github.com/spicyjack/wadtools/issues
+
+use strictures 1; # strict + warnings
+use Moo;
+use Log::Log4perl qw(:no_extra_logdie_message);
+use Data::Dumper;
+$Data::Dumper::Indent = 1;
+$Data::Dumper::Sortkeys = 1;
+$Data::Dumper::Terse = 1;
+
+sub db_request_update {
+    my $self = shift;
+    my $log = Log::Log4perl->get_logger(""); # "" = root logger
+    $log->warn(q(received 'db_request_update' callback call));
+}
+
+sub db_request_success {
+    my $self = shift;
+    my $log = Log::Log4perl->get_logger(""); # "" = root logger
+    $log->warn(q(received 'db_request_success' callback call));
+}
+
+sub db_request_failure {
+    my $self = shift;
+    my $log = Log::Log4perl->get_logger(""); # "" = root logger
+    $log->warn(q(received 'db_request_failure' callback call));
+}

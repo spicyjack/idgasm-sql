1530cf25c02e14230ed940a6ec7a4b378b62b466
Brian Manning <brian@xaoc.org>
2014-02-21 18:28:38 -0800

    Vote: added POD

--- App-WADTools/lib/App/WADTools/Vote.pm
+++ App-WADTools/lib/App/WADTools/Vote.pm
@@ -3,7 +3,18 @@
 ###############################
 package App::WADTools::Vote;
 
-=head1 App::WADTools::Vote
+=head1 NAME
+
+App::WADTools::Vote
+
+=head1 SYNOPSIS
+
+ my $review = App::WADTools::Vote->new(
+     text => $file_review->{text},
+     vote => $file_review->{vote},
+ );
+
+=head1 DESCRIPTION
 
 An individual vote for a file in the C<idGames Archive>.  The information in
 this object is read directly from C<idGames Archive>, and comes from the vote
@@ -77,12 +88,36 @@ has q(vote) => (
 
 =over
 
-=item BUILD() (aka 'new')
+=item new() (aka 'BUILD')
 
 Creates the L<App::WADTools::Vote> object, optionally with an error
 message.
 
 =back
+
+=head1 AUTHOR
+
+Brian Manning, C<< <brian at xaoc dot org> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to the GitHub issue tracker for
+this project:
+
+C<< <https://github.com/spicyjack/wadtools/issues> >>.
+
+=head1 SUPPORT
+
+You can find documentation for this script with the perldoc command.
+
+    perldoc App::WADTools::Vote
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright (c) 2013-2014 Brian Manning, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
 
 =cut
 

894a5c14788849d4a2d483bc7e393a3bab4a4cf3
Brian Manning <brian@xaoc.org>
2014-03-02 22:44:44 -0800

    App::WADTools: added full POD documentation

--- App-WADTools/lib/App/WADTools.pm
+++ App-WADTools/lib/App/WADTools.pm
@@ -1,15 +1,56 @@
-use strict;
-use warnings;
-
 #################
 # App::WADTools #
 #################
 package App::WADTools;
 
-=head1 App::WADTools;
+use strict;
+use warnings;
 
-Tools to work with databases or database metadata files created using the data
-from the C<idGames Archive>.
+=head1 NAME
+
+App::WADTools
+
+=head1 SYNOPSIS
+
+ # This module is not callable by other modules/scripts.
+
+=head1 DESCRIPTION
+
+Tools to work with WAD files, and/or databases/database metadata files created
+using the data from the
+C<idGames Archive API|http://www.doomworld.com/idgames/api/>.
+
+=head2 Attributes
+
+None.
+
+=head2 Methods
+
+None.
+
+=head1 AUTHOR
+
+Brian Manning, C<< <brian at xaoc dot org> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to the GitHub issue tracker for
+this project:
+
+C<< <https://github.com/spicyjack/wadtools/issues> >>.
+
+=head1 SUPPORT
+
+You can find documentation for this script with the perldoc command.
+
+    perldoc App::WADTools
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright (c) 2013-2014 Brian Manning, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
 
 =cut
 

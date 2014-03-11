a8aab3a274dce015da30fb378df775e74786d759
Brian Manning <brian@xaoc.org>
2014-03-11 13:00:46 -0700

    View/GenericCLI: issue #176; added a generic "View" object

---
+++ App-WADTools/lib/App/WADTools/View/GenericCLI.pm
@@ -0,0 +1,53 @@
+############################################
+# package App::WADTools::Views::GenericCLI #
+############################################
+package App::WADTools::Views::GenericCLI;
+
+### System Modules
+use 5.010;
+use Moo;
+
+=head1 NAME
+
+App::WADTools::Views::CLI::Generic
+
+=head1 SYNOPSIS
+
+ my $view = App::WADTools::Views::CLI::Generic->new();
+ my $controller = Some::Controller->new(view => $view);
+
+ # in the Some::Controller object...
+ $self->view->update_status(q(This is a status update));
+ $self->view->update_view(<information for the view to update>);
+
+=head1 DESCRIPTION
+
+A generic CLI "view" object, that will accept C<View> callbacks and write them
+to STDOUT.
+
+=head2 Attributes
+
+This object has no attributes.
+
+=head2 Methods
+
+=over
+
+=item update_status()
+
+Write a status message to C<STDOUT>.
+
+=back
+
+=cut
+
+sub update_status{
+    my $self = shift;
+    my %args = @_;
+    my $log = Log::Log4perl->get_logger(""); # "" = root logger
+
+    say q(Update: ) . $args{level} . q(:) . $args{type};
+    say q(Update: ) . $args{message};
+}
+
+1;

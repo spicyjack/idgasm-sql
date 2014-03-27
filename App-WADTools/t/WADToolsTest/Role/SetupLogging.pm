a5dafa758d4a1f7281d9ca3f768f94f7e7ef05f1
Brian Manning <brian@xaoc.org>
2014-03-27 12:52:56 -0700

    renamed: Role/LoggingSetup.pm -> Role/SetupLogging.pm
    - Renamed method to 'setup_logging' as well

--- App-WADTools/t/WADToolsTest/Role/SetupLogging.pm
+++ App-WADTools/t/WADToolsTest/Role/SetupLogging.pm
@@ -1,18 +1,11 @@
-#!perl -T
-
-package WADToolsTest::Logging;
+package WADToolsTest::Role::SetupLogging;
 # Copyright (c) 2014 Brian Manning <brian at xaoc dot org>
 # For support with this file, please file an issue on the GitHub issue tracker
 # for this project: https://github.com/spicyjack/wadtools/issues
 
-use strictures 1; # strict + warnings
+use Moo::Role;
 use File::Basename;
 use Log::Log4perl qw(:no_extra_logdie_message);
-use Moo::Role;
-#use Data::Dumper;
-#$Data::Dumper::Indent = 1;
-#$Data::Dumper::Sortkeys = 1;
-#$Data::Dumper::Terse = 1;
 
 sub setup_logging {
     my $dirname = dirname($0);

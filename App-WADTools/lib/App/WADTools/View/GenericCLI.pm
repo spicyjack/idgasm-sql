35df710d797acc40c44f8f759e0a9244b5f84648
Brian Manning <brian@xaoc.org>
2014-05-20 18:06:53 -0700

    GenericCLI: close #202: check for pipes/--colorize
    - When writing messages to STDOUT, check to see if the output should
      be colorized or not; if the script is not connected to a terminal,
      and `--colorize` is not used, then ANSI colorization will be
      suppressed

--- App-WADTools/lib/App/WADTools/View/GenericCLI.pm
+++ App-WADTools/lib/App/WADTools/View/GenericCLI.pm
@@ -34,12 +34,12 @@ App::WADTools::Views::CLI::Generic
 
 =head1 SYNOPSIS
 
- my $view = App::WADTools::Views::CLI::Generic->new();
+ my $view = App::WADTools::Views::CLI::Generic->new(cfg => $config);
  my $controller = Some::Controller->new(view => $view);
 
  # in the Some::Controller object...
- $self->view->update_status(q(This is a status update));
- $self->view->update_view(<information for the view to update>);
+ $self->view->update(q(This is a status update));
+ $self->view->request_success(q(Information about request success));
 
 =head1 DESCRIPTION
 
@@ -48,12 +48,65 @@ to STDOUT.
 
 =head2 Attributes
 
-This object has no attributes.
+=item config
+
+An L<App::WADTools::Config> object.
+
+=cut
+
+has q(config) => (
+    is  => q(ro),
+    isa => sub{ ref($_[0]) =~ /Config/ },
+);
+
+=item colorize
+
+A flag to tell the L<GenericCLI> object whether or not to colorize the output
+of the module.  When a L<GenericCLI> object is created, it checks the
+L<App::WADTools::Config> object to see if the C<--colorize> flag was used, as
+well as checking whether C<STDOUT> is connected to a terminal or a pipe, and
+sets this flag to C<1> (true, colorize) or C<0> (false, don't colorize)
+accordingly.
+
+=cut
+
+has q(colorize) => (
+    is      => q(rwp),
+    isa     => sub{ $_[0] =~ /[01]/ },
+    default => sub { 0 },
+);
 
 =head2 Methods
 
 =over
 
+=item new(cfg => $config)
+
+Creates a L<GenericCLI> object and returns it to the caller.
+
+Required arguments:
+
+=over
+
+=item cfg
+
+An L<App::WADTools::Config> object.  The L<Config> object is used to determine
+things like input and output files, file formats, output colorization, and so
+on.
+
+=back
+
+=cut
+
+sub BUILD {
+    my $self = shift;
+
+    my $cfg = $self->config;
+    if ( -t STDOUT || $cfg->defined(q(colorize)) ) {
+        # this is how you set attribs when "is => q(rwp)" is used
+        $self->_set_colorize(1);
+    }
+}
 =item update()
 
 Show an update of the current request to the user.  Accepts the following
@@ -80,15 +133,30 @@ sub update {
     my %args = @_;
 
     my ($level, $message);
-    if ( exists $args{lvl} ) {
+    if ( exists $args{lvl} && exists $_prefix{lvl} ) {
         $level = $args{lvl};
-    } elsif ( exists $args{level} ) {
+    } elsif ( exists $args{level} && exists $_prefix{level} ) {
         $level = $args{level};
     } else {
+        # set a default level of "info"
         $level = q(info);
     }
-    say colored([$_colors{$args{level}}],
-        $_prefix{$args{level}} . $args{message});
+
+    if ( exists $args{msg} ) {
+        $message = $args{msg};
+    } elsif ( exists $args{message} ) {
+        $message = $args{message};
+    } else {
+        # default message that hopefully warns the user that something's wrong
+        warn q(Received an 'update' call without a 'message' argument);
+        $message = q(Default update message);
+    }
+
+    if ( $self->colorize ) {
+        say colored([$_colors{$level}], $_prefix{$level} . $args{message});
+    } else {
+        say $_prefix{$level} . $args{message};
+    }
 }
 
 =item request_success()
@@ -101,8 +169,24 @@ sub request_success {
     my $self = shift;
     my %args = @_;
 
-    say colored([$_colors{success}], $args{message});
+    my $message;
+
+    if ( exists $args{msg} ) {
+        $message = $args{msg};
+    } elsif ( exists $args{message} ) {
+        $message = $args{message};
+    } else {
+        # default message that hopefully warns the user that something's wrong
+        warn q(Received an 'request_success' call without a 'message' argument);
+        $message = q(Default update message);
+    }
+
     #say q(Success! ) . $args{message};
+    if ( $self->colorize ) {
+        say colored([$_colors{success}], $message);
+    } else {
+        say $message;
+    }
 }
 
 =item request_failure()
@@ -118,8 +202,24 @@ sub request_failure {
     my $self = shift;
     my %args = @_;
 
-    say colored([$_colors{failure}], $args{message});
+    my $message;
+
+    if ( exists $args{msg} ) {
+        $message = $args{msg};
+    } elsif ( exists $args{message} ) {
+        $message = $args{message};
+    } else {
+        # default message that hopefully warns the user that something's wrong
+        warn q(Received an 'request_success' call without a 'message' argument);
+        $message = q(Default update message);
+    }
+
     #say q(Failure! ) . $args{message} . q| (| . $args{id} . q|)|;
+    if ( $self->colorize ) {
+        say colored([$_colors{failure}], $message);
+    } else {
+        say $message;
+    }
 }
 
 1;
